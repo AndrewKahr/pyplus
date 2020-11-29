2ec9c7b889cd900581cb7da270982c31f6a19ff3
Andrew <none@invalid.com>
2020-11-28 23:21:15 -0800

    Updated test script

--- tests/tester.py
+++ tests/tester.py
@@ -4,8 +4,7 @@ print("hello")
 # Useful Comment Test
 print("hello2")
 
-x = [5.21234234435435345345345345]
-print(type(x).__name__)
+x = 5.21234234435435345345345345
 
 x = b = 3
 
@@ -19,7 +18,13 @@ else:
 
 def print_test():
     print("Test")
+    y = 3
+    y = 8
+    y = 3 + 9
+    z = 3 - 9
 
 
 def abc(a, y, z):
     print("3342")
+    y = 6
+    y = 6
